2dfe28e400531b69ad21338b742f420d56093668
Vincenzo Maffione <v.maffione@gmail.com>
2021-01-20 17:00:15 +0100

    import basic structure

---
+++ src/utils.cpp
@@ -0,0 +1,560 @@
+/*
+ * utils
+ *
+ * Author: Vincenzo M. (2020)
+ */
+
+#include <algorithm>
+#include <cassert>
+#include <cstring>
+#include <dirent.h>
+#include <fcntl.h>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <netdb.h>
+#include <sys/eventfd.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <vector>
+
+#include <utils.hpp>
+
+namespace utils {
+
+bool
+DirExists(const char *path)
+{
+	struct stat st;
+
+	if (stat(path, &st)) {
+		return false;
+	}
+
+	return S_ISDIR(st.st_mode);
+}
+
+bool
+DirExists(const std::string &s)
+{
+	return DirExists(s.c_str());
+}
+
+bool
+DirEmpty(const std::string &dirpath)
+{
+	struct dirent *dent;
+	bool result = true;
+	DIR *dir;
+
+	dir = opendir(dirpath.c_str());
+	if (dir == nullptr) {
+		std::cerr << logb(LogErr) << "Failed to opendir(" << dirpath
+			  << "): " << strerror(errno) << std::endl;
+		return false;
+	}
+
+	while ((dent = readdir(dir)) != nullptr) {
+		if (strcmp(dent->d_name, ".") && strcmp(dent->d_name, "..")) {
+			result = false;
+			break;
+		}
+	}
+
+	closedir(dir);
+
+	return result;
+}
+
+bool
+IsDir(const char *path)
+{
+	return DirExists(path);
+}
+
+std::string
+CurrentDirectory()
+{
+	char buf[PATH_MAX];
+
+	if (getcwd(buf, sizeof(buf)) == nullptr) {
+		return std::string();
+	}
+
+	return std::string(buf);
+}
+
+bool
+IsFile(const char *path)
+{
+	struct stat st;
+
+	if (stat(path, &st)) {
+		return false;
+	}
+
+	return S_ISREG(st.st_mode);
+}
+
+bool
+IsFile(const std::string &path)
+{
+	return IsFile(path.c_str());
+}
+
+long long int
+FileSize(const char *path)
+{
+	struct stat st;
+	int ret = stat(path, &st);
+
+	if (ret) {
+		std::cerr << logb(LogErr) << "Failed to stat(" << path
+			  << "): " << strerror(errno) << std::endl;
+		return -1;
+	}
+
+	return static_cast<long long int>(st.st_size);
+}
+
+long long int
+FileSize(const std::string &filename)
+{
+	std::ifstream in(filename, std::ifstream::ate | std::ifstream::binary);
+
+	if (!in.good()) {
+		return 0;
+	}
+
+	return in.tellg();
+}
+
+std::string
+FileBaseName(const std::string &path)
+{
+	size_t slashpos = path.rfind('/');
+
+	if (slashpos == std::string::npos) {
+		return path;
+	}
+
+	return path.substr(slashpos + 1);
+}
+
+std::string
+FileParentDir(const std::string &path)
+{
+	size_t slashpos = path.rfind('/');
+
+	if (slashpos == std::string::npos) {
+		return std::string();
+	}
+
+	return path.substr(0, slashpos);
+}
+
+bool
+FileHasExtension(const std::string &filename, const std::string &ext)
+{
+	return filename.size() > ext.size() &&
+	       filename.substr(filename.size() - ext.size()) == ext;
+}
+
+static std::string
+CurtimeString()
+{
+	time_t ctime = time(nullptr);
+	struct tm *time_info;
+	char tbuf[9];
+
+	time_info = localtime(&ctime);
+	strftime(tbuf, sizeof(tbuf), "%H:%M:%S", time_info);
+
+	return std::string(tbuf);
+}
+
+std::string
+logb(int loglevel)
+{
+	std::string ret;
+
+	ret = std::string("[") + CurtimeString() + ":";
+
+	switch (loglevel) {
+	case LogErr:
+		ret += "ERR] ";
+		break;
+
+	case LogWrn:
+		ret += "WRN] ";
+		break;
+
+	case LogInf:
+		ret += "INF] ";
+		break;
+
+	case LogDbg:
+		ret += "DBG] ";
+		break;
+
+	default:
+		assert(false);
+		break;
+	}
+
+	return ret;
+}
+
+std::string
+PathJoin(const std::string &dirpath, const std::string &name)
+{
+	if (dirpath.empty()) {
+		return name;
+	}
+
+	std::string res = dirpath;
+
+	if (dirpath[dirpath.size() - 1] != '/') {
+		res += '/';
+	}
+
+	return res + name;
+}
+
+int
+MkdirIfNotExists(const std::string &path)
+{
+	int ret;
+
+	if (DirExists(path)) {
+		return 0;
+	}
+
+	ret = mkdir(path.c_str(), 0775);
+	if (ret) {
+		std::cerr << logb(LogErr) << "Failed to mkdir(" << path
+			  << "): " << strerror(errno) << std::endl;
+		return ret;
+	}
+
+	return 0;
+}
+
+std::string
+JoinStrings(const std::vector<std::string> &strings, const std::string &delim)
+{
+	std::stringstream ss;
+	bool first = true;
+
+	for (const std::string &s : strings) {
+		if (!first) {
+			ss << delim;
+		} else {
+			first = false;
+		}
+		ss << s;
+	}
+
+	return ss.str();
+}
+
+int
+CopyFile(const std::string &dstname, const std::string &srcname)
+{
+	UniqueFd src(open(srcname.c_str(), O_RDONLY, 0));
+	if (src < 0) {
+		std::cerr << "Failed to open " << srcname << ": "
+			  << strerror(errno) << std::endl
+			  << std::flush;
+		return src;
+	}
+
+	UniqueFd dst(open(dstname.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644));
+	if (dst < 0) {
+		std::cerr << "Failed to open " << dstname << ": "
+			  << strerror(errno) << std::endl
+			  << std::flush;
+		return dst;
+	}
+
+	char buf[128 * 1024];
+	ssize_t size;
+
+	while ((size = read(src, buf, sizeof(buf))) > 0) {
+		ssize_t ofs = 0;
+		ssize_t n;
+
+		if (size < 0) {
+			std::cerr << "Failed to read(" << srcname
+				  << "): " << strerror(errno) << std::endl
+				  << std::flush;
+			return size;
+		}
+
+		while (ofs < size &&
+		       (n = write(dst, buf + ofs, size - ofs)) >= 0) {
+			ofs += n;
+		}
+
+		if (n < 0) {
+			std::cerr << "Failed to write(" << dstname
+				  << "): " << strerror(errno) << std::endl
+				  << std::flush;
+			return n;
+		}
+	}
+
+	return 0;
+}
+
+int
+ExecuteCommand(std::stringstream &cmdss, bool verbose, bool daemonize)
+{
+	std::vector<std::string> tokens;
+	std::string token;
+	pid_t child_pid;
+	int child_status;
+	int ret = -1;
+	char **argv;
+
+	if (verbose) {
+		std::cout << logb(LogDbg) << "Exec command '" << cmdss.str()
+			  << "'" << std::endl;
+	}
+
+	/* Separate the arguments into a vector. */
+	while (cmdss >> token) {
+		tokens.push_back(token);
+	}
+
+	/* Allocate a working copy for the arguments. */
+	argv = new char *[tokens.size() + 1];
+	for (unsigned i = 0; i <= tokens.size(); i++) {
+		argv[i] = nullptr;
+	}
+	for (unsigned i = 0; i < tokens.size(); i++) {
+		argv[i] = strdup(tokens[i].c_str());
+		if (!argv[i]) {
+			std::cerr
+			    << "ExecuteCommand: out of memory while allocating "
+			       "arguments"
+			    << std::endl;
+			goto out;
+		}
+	}
+
+	child_pid = fork();
+	if (child_pid == 0) {
+		/* Child process: Redirect standard input, output and
+		 * error to /dev/null and execute the target command. */
+
+		close(0);
+		close(1);
+		close(2);
+		if (open("/dev/null", O_RDONLY) < 0 ||
+		    open("/dev/null", O_WRONLY) < 0 ||
+		    open("/dev/null", O_WRONLY) < 0) {
+			/* Redirection failed. */
+			return -1;
+		}
+		execvp(argv[0], argv);
+		perror("execvp()");
+		exit(127); /* command not found */
+	}
+
+	if (daemonize) {
+		ret = static_cast<int>(child_pid);
+	} else {
+		/* Parent process. Wait for the child to exit. */
+		waitpid(child_pid, &child_status, 0);
+		if (WIFEXITED(child_status)) {
+			ret = WEXITSTATUS(child_status);
+			ret *= -1;
+		}
+	}
+
+out:
+	for (unsigned i = 0; i < tokens.size(); i++) {
+		if (argv[i]) {
+			free(argv[i]);
+			argv[i] = nullptr;
+		}
+	}
+	delete[] argv;
+
+	if (verbose && ret == -127) {
+		std::cerr << "ExecuteCommand: command not found" << std::endl;
+	}
+
+	return ret;
+}
+
+int
+ExecuteCommand(const std::string &cmdstring, bool verbose, bool daemonize)
+{
+	std::stringstream cmdss(cmdstring);
+
+	return ExecuteCommand(cmdss, verbose, daemonize);
+}
+
+int
+FdSetBlocking(int fd, bool blocking)
+{
+	int flags = fcntl(fd, F_GETFL, 0);
+
+	if (flags == -1) {
+		std::cerr << logb(LogErr) << "fcntl(F_GETFL) failed"
+			  << std::endl;
+
+		return flags;
+	}
+
+	if (blocking) {
+		flags &= ~O_NONBLOCK;
+	} else {
+		flags |= O_NONBLOCK;
+	}
+
+	flags = fcntl(fd, F_SETFL, flags);
+	if (flags < 0) {
+		std::cerr << logb(LogErr) << "fcntl(F_GETFL) failed"
+			  << std::endl;
+
+		return flags;
+	}
+
+	return 0;
+}
+
+int
+EventFdSignal(int efd)
+{
+	uint64_t val = 1;
+	int n;
+
+	n = write(efd, &val, sizeof(val));
+	if (n < 0) {
+		std::cerr << logb(LogErr)
+			  << "Failed to notify stopfd: " << strerror(errno)
+			  << std::endl;
+		return n;
+
+	} else if (n != sizeof(val)) {
+		std::cerr << logb(LogErr) << "Incomplete write to stopfd: " << n
+			  << "/" << sizeof(val) << std::endl;
+		return -1;
+	}
+
+	return 0;
+}
+
+int
+EventFdDrain(int efd)
+{
+	uint64_t val;
+	int n;
+
+	n = read(efd, &val, sizeof(val));
+	if (n < 0) {
+		std::cerr << logb(LogErr)
+			  << "Failed to drain eventfd(): " << strerror(errno)
+			  << std::endl;
+		return n;
+
+	} else if (n != sizeof(val)) {
+		std::cerr << logb(LogErr)
+			  << "Incomplete read from stopfd: " << n << "/"
+			  << sizeof(val) << std::endl;
+		return -1;
+	}
+
+	return 0;
+}
+
+IPAddr::IPAddr(const std::string &_p) : repr(_p)
+{
+	std::string p = _p;
+	std::string digit;
+	size_t slash;
+	int m;
+
+	slash = p.find("/");
+
+	if (slash != std::string::npos) {
+		/* Extract the mask m in "a.b.c.d/m" */
+		if (!Str2Num<int>(p.substr(slash + 1), m)) {
+			throw "Invalid IP prefix";
+		}
+		if (m < 1 || m > 32) {
+			throw "Invalid IP prefix";
+		}
+		netbits = m;
+		p	= p.substr(0, slash);
+	} else {
+		/* No mask info. Assume /32. */
+		netbits = 32;
+	}
+
+	/* Extract a, b, c and d. */
+	std::replace(p.begin(), p.end(), '.', ' ');
+
+	std::stringstream ss(p);
+
+	addr = 0;
+	while (ss >> digit) {
+		int d;
+
+		if (!Str2Num<int>(digit, d)) {
+			throw "Invalid IP prefix";
+		}
+
+		if (d < 0 || d > 255) {
+			throw "Invalid IP prefix";
+		}
+		addr <<= 8;
+		addr |= (unsigned)d;
+	}
+
+	return;
+}
+
+IPAddr::IPAddr(uint32_t naddr, unsigned nbits)
+{
+	unsigned a, b, c, d;
+	std::stringstream ss;
+
+	if (!nbits || nbits > 30) {
+		throw "Invalid IP prefix";
+	}
+
+	addr	= naddr;
+	netbits = nbits;
+
+	d = naddr & 0xff;
+	naddr >>= 8;
+	c = naddr & 0xff;
+	naddr >>= 8;
+	b = naddr & 0xff;
+	naddr >>= 8;
+	a = naddr & 0xff;
+
+	ss << a << "." << b << "." << c << "." << d << "/" << nbits;
+	repr = ss.str();
+}
+
+std::string
+IPAddr::StrNoPrefix() const
+{
+	size_t slash = repr.find("/");
+
+	if (slash == std::string::npos) {
+		return repr;
+	}
+
+	return repr.substr(0, slash);
+}
+
+} // namespace utils
---
+++ src/utils.hpp
@@ -0,0 +1,163 @@
+/*
+ * Author: Vincenzo M. (2020)
+ */
+
+#ifndef __UTILS_HPP__
+#define __UTILS_HPP__
+
+#include <arpa/inet.h>
+#include <string>
+#include <sstream>
+#include <unistd.h>
+#include <vector>
+
+namespace utils {
+
+enum {
+	LogErr = 0,
+	LogWrn,
+	LogInf,
+	LogDbg,
+};
+
+bool DirExists(const char *path);
+bool DirExists(const std::string &s);
+bool DirEmpty(const std::string &dirpath);
+bool IsDir(const char *path);
+std::string CurrentDirectory();
+bool IsFile(const char *path);
+bool IsFile(const std::string &path);
+long long int FileSize(const char *path);
+long long int FileSize(const std::string &filename);
+std::string FileBaseName(const std::string &path);
+std::string FileParentDir(const std::string &path);
+bool FileHasExtension(const std::string &filename, const std::string &ext);
+std::string PathJoin(const std::string &dirpath, const std::string &name);
+int MkdirIfNotExists(const std::string &path);
+std::string JoinStrings(const std::vector<std::string> &strings,
+			const std::string &delim);
+int CopyFile(const std::string &dstname, const std::string &srcname);
+
+int ExecuteCommand(std::stringstream &cmdss, bool verbose = false,
+		   bool daemonize = false);
+int ExecuteCommand(const std::string &cmdstring, bool verbose = false,
+		   bool daemonize = false);
+
+int FdSetBlocking(int fd, bool blocking);
+int EventFdSignal(int efd);
+int EventFdDrain(int efd);
+
+std::string logb(int loglevel);
+
+/* Helper class to manage file descriptors with RAII. */
+class UniqueFd {
+	int fd = -1;
+
+    public:
+	UniqueFd() : UniqueFd(-1) {}
+	UniqueFd(int fd) : fd(fd) {}
+	UniqueFd(const UniqueFd &) = delete;
+	UniqueFd &operator=(const UniqueFd &) = delete;
+
+	UniqueFd(UniqueFd &&o)
+	{
+		reset();
+		fd   = o.fd;
+		o.fd = -1;
+	}
+
+	UniqueFd &operator=(UniqueFd &&o)
+	{
+		if (&o != this) {
+			reset();
+			fd   = o.fd;
+			o.fd = -1;
+		}
+
+		return *this;
+	}
+
+	~UniqueFd() { reset(); }
+
+	int get() const { return fd; }
+	operator int() const { return fd; }
+	bool empty() const { return fd < 0; }
+
+	void reset()
+	{
+		if (fd >= 0) {
+			close(fd);
+			fd = -1;
+		}
+	}
+};
+
+template <class T, bool HEX = false>
+bool
+Str2Num(const std::string &s, T &num)
+{
+	std::istringstream iss(s);
+
+	if (HEX) {
+		iss >> std::hex >> num;
+	} else {
+		iss >> num;
+	}
+
+	return !iss.fail();
+}
+
+template <class T, bool HEX = false>
+std::string
+Num2Str(T num)
+{
+	std::ostringstream oss;
+
+	if (HEX) {
+		oss << std::hex << num;
+	} else {
+		oss << num;
+	}
+
+	return oss.str();
+}
+
+struct IPAddr {
+	std::string repr;
+	uint32_t addr;
+	unsigned netbits;
+
+	IPAddr() : addr(0), netbits(0) {}
+	IPAddr(const std::string &p);
+	IPAddr(uint32_t naddr, unsigned nbits);
+
+	bool IsEmpty() const { return netbits == 0 && addr == 0; }
+
+	bool operator<(const IPAddr &o) const
+	{
+		return addr < o.addr || (addr == o.addr && netbits < o.netbits);
+	}
+
+	/* Pick an host address in belonging to the same subnet. */
+	IPAddr HostAddr(uint32_t host, uint32_t new_netbits = -1) const
+	{
+		uint32_t hostmask = ~((1 << (32 - netbits)) - 1);
+
+		if (host & hostmask) {
+			throw "Host number out of range";
+		}
+
+		if (new_netbits == static_cast<uint32_t>(-1)) {
+			new_netbits = netbits;
+		}
+
+		return IPAddr((addr & hostmask) + host, new_netbits);
+	}
+
+	std::string StrNoPrefix() const;
+	operator std::string() const { return repr; }
+};
+
+} // namespace utils
+
+#endif /* __UTILS_HPP__ */
